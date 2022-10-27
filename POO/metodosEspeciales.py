aa58c225a6f3557620064fbaa428bc2e599dd04f
Hi-i-am-Akatosh <da.vifer26@hotmail.com>
2022-10-27 17:59:36 -0500

    Metodos especiales en python

---
+++ POO/metodosEspeciales.py
@@ -0,0 +1,24 @@
+from ast import AugStore
+
+
+class Libro:
+    def __init__(self, titulo, autor, paginas):
+        print("Un libro esta creado")
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def __str__(self):
+        return "Titulo: %s, autor: %s, paginas: %s"%(self.titulo, self.autor,self.paginas)
+
+    def __len__(self):
+        return self.paginas
+
+    def __del__(self):
+        print("Un libro eliminado")
+
+libro = Libro("Libro Python", "David", 200)
+
+#Impresion de los metodos especiales
+print(libro)
+
