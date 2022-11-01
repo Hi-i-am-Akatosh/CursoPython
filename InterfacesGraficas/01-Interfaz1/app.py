61a546490a8413ec33968e94691f15dceae53dc2
Hi-i-am-Akatosh <da.vifer26@hotmail.com>
2022-11-01 18:10:28 -0500

    Entrada a interfaces graficas

---
+++ InterfacesGraficas/01-Interfaz1/app.py
@@ -0,0 +1,21 @@
+"""import tkinter as tk
+raiz = tk.Tk()
+
+raiz.mainloop()"""
+
+from tkinter import *
+raiz=Tk()
+#Cambiar el nombre de la ventana.
+raiz.title("Primera Ventana")
+#Configuracion de tamaño
+raiz.geometry("520x480")
+#Cambiar icono
+#raiz.iconbitmap("C:\Users\USUARIO\Desktop")
+#Cambiar el color de fondo
+raiz.config(bg="skyblue")
+#No permite reajustar el tamaño del aplicativo
+raiz.resizable(0,0)
+
+#Ejecuta la ventana
+raiz.mainloop()
+
