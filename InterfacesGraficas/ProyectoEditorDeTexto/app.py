46e9ff6c41a9d3522b3b0aad2c082082e0076b67
Hi-i-am-Akatosh <da.vifer26@hotmail.com>
2022-11-15 17:43:34 -0500

    Proyecto Blog de notas Python

--- InterfacesGraficas/ProyectoEditorDeTexto/app.py
+++ InterfacesGraficas/ProyectoEditorDeTexto/app.py
@@ -5,20 +5,72 @@ from io import open
 
 ruta = "" #almacenar la ruta del fichero
 
+
+#Agregar metodos
+def nuevo():
+    global ruta
+    mensaje.set("Nuevo fichero")
+    ruta = ""
+    texto.delete(1.0, "end")
+    root.title("Mi editor")
+
+def abrir():
+    global ruta
+    mensaje.set("Abrir fichero")
+    ruta = FileDialog.askopenfilename(
+        initialdir='.',
+        filetypes=(("Fichero de texto", "*.txt"),),
+        title="Abrir un fichero de texto")
+
+    if ruta != "":
+            fichero = open(ruta, 'r')
+            contenido = fichero.read()
+            texto.delete(1.0, 'end')
+            texto.insert('insert', contenido)
+            fichero.close()
+            root.title(ruta + " - Mi editor")        
+        
+def guardar():
+    mensaje.set("Guardar fichero")
+    if ruta != "":
+        contenido = texto.get(1.0, 'end-1c')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set("Fichero guardado correctamente")
+    else:
+        print("Guardar como ...")
+
+def guardar_como():
+    global ruta
+    mensaje.set("Guardar como ...")
+
+    fichero = FileDialog.asksaveasfile(title="Guardar fichero", mode = "w", defaultextension=".txt")
+
+    if fichero is not None:
+        ruta = fichero.name
+        contenido = texto.get(1.0 , 'end-1c')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set("Guardado cancelado")
+        ruta = ""
+
 root = Tk()
 root.title("Mi Editor")
 
 #Menu Superior
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Nuevo")
-filemenu.add_command(label="Abrir")
-filemenu.add_command(label="Guardar")
-filemenu.add_command(label="Guardar como")
+filemenu.add_command(label="Nuevo", command=nuevo)
+filemenu.add_command(label="Abrir", command=abrir)
+filemenu.add_command(label="Guardar", command=guardar )
+filemenu.add_command(label="Guardar como", command=guardar_como)
 filemenu.add_separator()
 filemenu.add_command(label="Salir", command=root.quit)
 menubar.add_cascade(label="Archivo", menu=filemenu)
 #Caja de texto central
+
 texto = Text(root)
 texto.pack(fill='both', expand=1)
 texto.config(padx=6,  pady=4, bd=0, font=("Consolas", 12))
