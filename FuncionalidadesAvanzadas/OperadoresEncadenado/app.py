0a968707d8424138134737f086b5973a08529cdb
Hi-i-am-Akatosh <da.vifer26@hotmail.com>
2022-11-16 17:04:15 -0500

    Funciones avanzadas Python

---
+++ FuncionalidadesAvanzadas/OperadoresEncadenado/app.py
@@ -0,0 +1,16 @@
+numero = 110
+
+if numero >= 0 and numero <= 100:
+  print("El numero {} se encuentra entre 0 y 100".format(numero))
+else:
+  print("El numero {} no se encuentra entre 0 y 100".format(numero))
+
+
+#operadores encadenados
+
+numero = 35
+
+if 0 <= numero <= 100:
+  print("El numero {} se encuentra entre 0 y 100".format(numero))
+else:
+  print("El numero {} se encuentra entre 0 y 100".format(numero))  
